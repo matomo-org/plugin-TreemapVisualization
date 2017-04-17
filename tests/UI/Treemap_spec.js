0f5bbcfc2b0a843451a014c1620aab0551630b40
sgiehl <stefan@piwik.org>
2017-04-17 21:18:00 +0200

    improve tests

--- tests/UI/Treemap_spec.js
+++ tests/UI/Treemap_spec.js
@@ -11,7 +11,7 @@ describe("Treemap", function () {
     this.timeout(0);
 
     var generalParams = 'idSite=1&period=year&date=2012-08-09',
-        urlBase = 'module=CoreHome&action=index&' + generalParams,
+        urlBase = 'module=CoreHome&action=index&token_auth=9ad1de7f8b329ab919d854c556f860c1&' + generalParams,
         normalUrl = "?module=Widgetize&action=iframe&moduleToWidgetize=DevicesDetection&idSite=1&period=year&date=2012-08-09&"
                   + "actionToWidgetize=getBrowsers&viewDataTable=table&filter_limit=5&isFooterExpandedInDashboard=1",
         actionsUrl = "?" + urlBase + "#?" + generalParams + "&category=General_Actions&subcategory=General_Pages"
